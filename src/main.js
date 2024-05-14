3367e6598111ee8becc22ba90813885631e3e41c
BefikerG <befikergezahegn196@gmail.com>
2024-05-14 23:42:06 +0300

    Homepage final touch

---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+
+// JavaScript to hide the loader and show the main content
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.getElementById("loader");
+    const mainContent = document.getElementById("main-content");
+
+    // Delay for the loader to display before hiding
+    setTimeout(function() {
+        // Fade out loader
+        loader.classList.add("animate-fadeOut");
+
+        // Fade in main content after loader fades out
+        setTimeout(function() {
+            loader.style.display = "none"; // Ensure loader is hidden after fade out
+            mainContent.classList.remove("opacity-0"); // Remove initial opacity
+            mainContent.classList.add("animate-fadeIn");
+        }, 1000); // Duration of the fade-out animation
+    }, 3000); // Adjust the timeout duration as needed
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.html"],
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+            "./src/**/*.html", 
+            "./src/**/*.js"
+  ],        
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', visibility: 'hidden' },
+          '100%': { opacity: '1', visibility: 'visible' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1', visibility: 'visible' },
+          '100%': { opacity: '0', visibility: 'hidden' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in forwards',
+        fadeOut: 'fadeOut 1s ease-out forwards',
+      },
+    },
   },
   plugins: [],
-}
-
+});
